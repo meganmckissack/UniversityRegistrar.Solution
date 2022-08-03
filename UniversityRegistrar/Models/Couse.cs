86f52934b05bd5e97b3062a3c3b76a594038a59c
Megan McKissack <meganmckissack@gmail.com>
2022-08-02 19:54:17 -0700

    create initial classes and update namespaces

---
+++ UniversityRegistrar/Models/Couse.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace UniversityRegistrar.Models 
+{
+  public class Course
+  {
+    public string CourseName { get; set; }
+    public string CourseNumber { get; set; }
+  }
+}
