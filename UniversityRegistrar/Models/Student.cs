d07761ca80e81e70b0c11a2bcacccce0d1f2fe00
Megan McKissack <meganmckissack@gmail.com>
2022-08-03 20:22:57 -0700

    create student views, controllers, and fix errors

--- UniversityRegistrar/Models/Student.cs
+++ UniversityRegistrar/Models/Student.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 namespace UniversityRegistrar.Models
 {
     public class Student
     {
+      public Student()
+      {
+        this.JoinEntities = new HashSet<CourseStudent>();
+      }
         public int StudentId { get; set; }
         public string StudentName { get; set; }
         public DateTime EnrollDate { get; set; }
+        public virtual ICollection<CourseStudent> JoinEntities { get;}
     }
 }
